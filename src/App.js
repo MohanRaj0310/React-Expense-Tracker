49b64a1284bf21d75ebc4afa389cbf700a4c8b1d
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-10-07 08:14:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <h1>something new baby</h1>
+      <ExpenseItem></ExpenseItem>
+      <button>start</button>
+    </div>
+  );
+}
+export default App;
+
+
+
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem (){
+  return (
+    <div>
+      <h1>ExpenseItem here!!!</h1>
+      <h2>ffioskwl</h2>
+    </div>
+  )
+}
+
+export default ExpenseItem
