725223a9115346f38f5ecd072a3ed394c540c096
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-10-09 16:48:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
-import "./components/ExpenseItem.css";
+import Expenses from "./components/Expenses";
 const Template = function (props){
   console.log(arguments)
   return (
@@ -38,22 +37,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem 
-        title={expenses[0].title} 
-        amount={expenses[0].amount}
-        date={expenses[0].date}/>
-      <ExpenseItem 
-        title={expenses[1].title} 
-        amount={expenses[1].amount}
-        date={expenses[1].date}/>
-      <ExpenseItem 
-        title={expenses[2].title} 
-        amount={expenses[2].amount}
-        date={expenses[2].date}/>
-      <ExpenseItem 
-        title={expenses[3].title} 
-        amount={expenses[3].amount}
-        date={expenses[3].date}/>
+      <Expenses expenses={expenses}/>
       <Template title={expenses[2].title}/>
       {/*<Template title={expenses.forEach((userInfo)=> {
         return userInfo
