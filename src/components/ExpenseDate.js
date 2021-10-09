661c96abf0ca47e8cb9c901f22adf2c2054b2a35
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-10-09 13:08:20 +0530

    expensesDate componenent is created

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,14 @@
+import './ExpenseDate.css'
 function ExpenseDate (props){
   const month = props.date.toLocaleString('en-gb', { month: 'long'})
   const day = props.date.toLocaleString('en-gb', {day: '2-digit'})
   const year = props.date.getFullYear()
-  console.log(props)
+  console.log(props)//{date: Wed Jun 16 2021 00:00:00 GMT+0530 (India Standard Time)}..so on .......
   return (
-    <div>
-      <div>{day}</div>
-      <div>{month}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   )
 }
